1ab6e6a6bebeba36e04e806e632c63192f7c48c8
alexisbarberis0 <alexis.barberis@etu.univ-amu.fr>
2026-06-08 00:02:58 +0200

    refactor(db): rename air_conditioning_planner to preconditioning_planner

--- db/script_creation_app.sql
+++ db/script_creation_app.sql
@@ -1,5 +1,5 @@
 ------------------------------------------------------------------
---                          VERSION 10                          --
+--                          VERSION 11                          --
 ------------------------------------------------------------------
 
 DROP TABLE IF EXISTS app.vehicles CASCADE;
@@ -8,9 +8,9 @@ DROP TABLE IF EXISTS app.oauth2_token CASCADE;
 DROP TABLE IF EXISTS app.vehicle_models CASCADE;
 DROP TABLE IF EXISTS app.paths CASCADE;
 DROP TABLE IF EXISTS app.path_points CASCADE;
-DROP TABLE IF EXISTS app.air_conditioning_planner CASCADE;
+DROP TABLE IF EXISTS app.preconditioning_planner CASCADE;
 DROP TABLE IF EXISTS app.charging_planner CASCADE;
-DROP TABLE IF EXISTS app.ac_plans CASCADE;
+DROP TABLE IF EXISTS app.preconditioning_plans CASCADE;
 DROP TABLE IF EXISTS app.charging_plans CASCADE;
 DROP TABLE IF EXISTS app.day_of_week CASCADE;
 DROP TABLE IF EXISTS app.jwt CASCADE;
@@ -131,16 +131,16 @@ CREATE TABLE app.path_points
         ON DELETE CASCADE
 );
 
-CREATE TABLE app.air_conditioning_planner
+CREATE TABLE app.preconditioning_planner
 (
     id                       UUID,
     vin                      VARCHAR(17) NOT NULL,
     activation_hour          TIME        NOT NULL,
     deactivate_after_success BOOLEAN,
 
-    CONSTRAINT pk_air_conditioning_planner PRIMARY KEY (id),
+    CONSTRAINT pk_preconditioning_planner PRIMARY KEY (id),
 
-    CONSTRAINT fk_air_conditioning_planner_vehicle FOREIGN KEY (vin)
+    CONSTRAINT fk_preconditioning_planner_vehicle FOREIGN KEY (vin)
         REFERENCES app.vehicles (vin)
         ON DELETE CASCADE
 );
@@ -170,18 +170,18 @@ CREATE TABLE app.day_of_week
     CONSTRAINT pk_day_of_week PRIMARY KEY (id)
 );
 
-CREATE TABLE app.ac_plans
+CREATE TABLE app.preconditioning_plans
 (
     id     UUID,
     day_id INT,
 
-    CONSTRAINT pk_ac_plans PRIMARY KEY (id, day_id),
+    CONSTRAINT pk_preconditioning_plans PRIMARY KEY (id, day_id),
 
-    CONSTRAINT fk_ac_plans_air_conditioning_planner FOREIGN KEY (id)
-        REFERENCES app.air_conditioning_planner (id)
+    CONSTRAINT fk_preconditioning_plans_preconditioning_planner FOREIGN KEY (id)
+        REFERENCES app.preconditioning_planner (id)
         ON DELETE CASCADE,
 
-    CONSTRAINT fk_ac_plans_day_of_week FOREIGN KEY (day_id)
+    CONSTRAINT fk_preconditioning_plans_day_of_week FOREIGN KEY (day_id)
         REFERENCES app.day_of_week (id)
         ON DELETE CASCADE
 );
