eb212e82cec9636478b607a8873bc70a8a2d3027
TCG-Blob <oriane.mejean@etu.univ-amu.fr>
2026-06-09 22:04:52 +0200

    add of the documentation

--- private/Models/Climate/ValueObjects/Temperature.php
+++ private/Models/Climate/ValueObjects/Temperature.php
@@ -5,10 +5,17 @@ namespace Teslapp\Models\Climate\ValueObjects;
 
 use InvalidArgumentException;
 
+/**
+ * Value object representing a valid cabin temperature
+ * Range: 15°C to 28°C
+ **/
 final readonly class Temperature
 {
     public float $value;
 
+    /**
+     * @throws InvalidArgumentException if the temperature is outside the accepted range
+     **/
     public function __construct(float $value)
     {
         if ($value < 15.0 || $value > 28.0) {
